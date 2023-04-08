5a509d890a6e1e38735dd1d2590e05d0dd6cfb50
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2023-04-08 17:59:48 +0200

    base preparation selectors, libraries

--- src/index.js
+++ src/index.js
@@ -2,5 +2,26 @@
 //
 //Style import
 import './css/styles.css';
-
+//
+//Funtion import
+import { fetchCountries } from './fetchCountries';
+//
+//Libraries import
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const debounce = require('lodash.debounce');
+//
+//Notifications settings
+Notiflix.Notify.init({
+  width: '300px',
+  position: 'right-top',
+  distance: '40px',
+  opacity: 1,
+});
+//
+//Base elements
 const DEBOUNCE_DELAY = 300;
+//
+//Selectors
+const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
